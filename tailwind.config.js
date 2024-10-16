76b8ccdae1ba019ca8553f484295c56634256aaf
Moahmmed Gomaa <mohammedgomaa2522@gmail.com>
2024-10-17 00:23:24 +0300

    add more products-v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#b3a781',
-        dark: '#302f2d',
-        light: '#d2c6b0',
-        light_pro: '#fffae7',
-        main_bg: '#1c1c5650',
+        primary: '#e4e0e1',
+        dark: '#e4e0e1',
+        light: '#e4e0e1',
+        light_pro: '#e4e0e1',
+        main_bg: '#e4e0e1',
       },
 
     },
